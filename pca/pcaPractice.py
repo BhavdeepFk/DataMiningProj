f83c6c2092bed1d0e0c7495b20bc4f56f0336ea7
Bhavdeep Sethi <sethibhavdeep@gmail.com>
2015-01-25 15:33:46 -0500

    Adding pca practice code

---
+++ pca/pcaPractice.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import plotly.plotly as py
+from plotly.graph_objs import *
+import plotly.tools as tls
+
+
+df = pd.read_csv(
+    filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', 
+    header=None, 
+    sep=',')
+
+df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
+df.dropna(how="all", inplace=True) # drops the empty line at file-end
+
+print df.tail()
+
+X = df.ix[:,0:4].values
+y = df.ix[:,4].values
+
+X_std = StandardScaler().fit_transform(X)
+
+'''
+mean_vec = np.mean(X_std, axis=0)
+cov_mat = (X_std - mean_vec).T.dot((X_std - mean_vec)) / (X_std.shape[0]-1)
+print('Covariance matrix \n%s' %cov_mat)
+'''
+
+cov_mat = np.cov(X_std.T)
+
+print('NumPy covariance matrix: \n%s' %cov_mat)
+
+eig_vals, eig_vecs = np.linalg.eig(cov_mat)
+
+print('Eigenvectors \n%s' %eig_vecs)
+print('\nEigenvalues \n%s' %eig_vals)
+
+for ev in eig_vecs:
+    np.testing.assert_array_almost_equal(1.0, np.linalg.norm(ev))
+print('Everything ok!')
+
+# Make a list of (eigenvalue, eigenvector) tuples
+eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
+
+# Sort the (eigenvalue, eigenvector) tuples from high to low
+eig_pairs.sort()
+eig_pairs.reverse()
+
+# Visually confirm that the list is correctly sorted by decreasing eigenvalues
+print('Eigenvalues in descending order:')
+for i in eig_pairs:
+    print(i[0])
+
+matrix_w = np.hstack((eig_pairs[0][1].reshape(4,1), 
+                      eig_pairs[1][1].reshape(4,1)))
+
+print('Matrix W:\n', matrix_w)
+
+Y = X_std.dot(matrix_w)
+
+
+traces = []
+
+for name in ('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'):
+
+    trace = Scatter(
+        x=Y[y==name,0],
+        y=Y[y==name,1],
+        mode='markers',
+        name=name,
+        marker=Marker(
+            size=12,
+            line=Line(
+                color='rgba(217, 217, 217, 0.14)',
+                width=0.5),
+            opacity=0.8))
+    traces.append(trace)
+
+
+data = Data(traces)
+layout = Layout(showlegend=True,
+                scene=Scene(xaxis=XAxis(title='PC1'),
+                yaxis=YAxis(title='PC2'),))
+
+fig = Figure(data=data, layout=layout)
+py.iplot(fig)    
